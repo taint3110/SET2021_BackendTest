b69c6ff96e1f0be050aa322a73f3c860ab92f029
Thành Tài <taint0812@gmail.com>
2022-03-13 20:47:38 +0700

    BET-01 | fixing Algorithm

--- Algorithm/index.js
+++ Algorithm/index.js
@@ -1,63 +1,99 @@
 const {cases} = require('./cases')
 const {NOTIFY_MESSAGE} = require('./notify-message')
+const firstCase = cases.firstCase
+const secondCase = cases.secondCase
+const thirdCase = cases.thirdCase
 
-function selectContainers(iCase = {}){
-  const rentedContainers = optimizeRentedContainer(iCase);
-  return rentedContainers
-}
+function optimizeSolution(contractsList, neededContainer = 0){
+  contractsList =  Array.isArray(contractsList) ? contractsList : []
+  const containersList = []
+  const possibleSolutions = []
+  contractsList.forEach(contract => {
+    containersList.push(contract.container)
+  }) 
+  let currentContainer = containersList[0]
 
-function optimizeRentedContainer(iCase = {}){
-  const containerList =  Array.isArray(iCase.listings) ? iCase.listings : []
-  const neededContainer = iCase.neededContainer;
-  const rentedContainers = {}
-  const allSolutions = []
-  // container.point = container.container / container.totalCost * 100 - container.container * 10
-  let minContainer = containerList[0].container
-  let maxContainer = containerList[0].container
-  containerList.forEach((container) => {
-    if (minContainer > container.container){
-      minContainer = container.container
+  containersList.forEach((container, index) => {
+    if (container === neededContainer) {
+      possibleSolutions.push({
+        container: container,
+        positions: index,
+        price: contractsList[index].totalCost
+      })
+      currentContainer = 0
     }
-    if (maxContainer < container.container){
-      maxContainer = container.container
+
+    if (container < neededContainer) {
+      currentContainer = container
+      let positions = [index]
+
+      for(let position = index; position < containersList.length - 1; position++){
+        currentContainer += containersList[position+1]
+        if (currentContainer === neededContainer) {
+          positions.push(position + 1)
+          let price = 0
+          positions.forEach(position => {
+            price += contractsList[position].totalCost
+          })
+          possibleSolutions.push({
+            container: currentContainer,
+            positions: positions,
+            price: price
+          })
+          positions = []
+        }
+        positions.push(position + 1)
+
+        if (currentContainer > neededContainer) {
+          currentContainer -= container
+          positions = [index + 1]
+        }
+
+        if(positions.length == containersList.length && possibleSolutions.length === 0 && currentContainer < neededContainer){
+          let price = 0
+          positions.forEach(position => {
+            price += contractsList[position].totalCost
+          })
+          possibleSolutions.push({
+            container: currentContainer,
+            positions: positions,
+            price: price
+          })
+        }
+      }
     }
   })
-  let solution = resetSolution()
-  if (minContainer <= neededContainer){
-    // const loopTime = (neededContainer / minContainer).toFixed
-    // const loops = new Array(loopTime).fill(0)
-    containerList.forEach((container, index) => {
-      console.log(solution)
-      solution.containerType[index] += 1
-      solution.containerAmount += container.container
-      solution.totalCost += container.totalCost
-      // allSolutions.push(index)
-      
-      containerList.forEach((container2, index2) => {
-        console.log(solution)
-        solution.containerAmount += container2.container
-        solution.totalCost += container2.totalCost
-        solution.containerType[index2] += 1
-    
-        allSolutions.push(solution)
-      })
+  if(Array.isArray(possibleSolutions) && possibleSolutions.length > 0){
+    let lowestPrice = possibleSolutions[0].price
+    possibleSolutions.forEach(solution => {
+      if(lowestPrice > solution.price){
+        lowestPrice = solution.price
+      }
+    })
+    possibleSolutions.forEach(solution => {
+      if(solution.price == lowestPrice){
+        return solution
+      }
     })
   }
+  return possibleSolutions[0]
 }
 
-
-function resetSolution(){
-  return {
-    containerAmount: 0,
-    totalCost: 0,
-    containerType: [
-      0, 0 ,0
-    ]
+function calculateSolution(contractsList, neededContainer){
+  const bestSolution = optimizeSolution(contractsList, neededContainer)
+  const positions = Array.isArray(bestSolution.positions) ? bestSolution.positions : [bestSolution.positions]
+  positions.forEach(position => {
+    let name = contractsList[position].name
+    let container = contractsList[position].container
+    let price = contractsList[position].totalCost
+    console.log(`${NOTIFY_MESSAGE.CONTRACT_WITH} ${name} ${container}, ${NOTIFY_MESSAGE.PRICE} ${price}`)
+  })
+  if(bestSolution.container < neededContainer){
+    console.log(NOTIFY_MESSAGE.NOT_ENOUGH_CONTAINER)
   }
+  console.log(`${NOTIFY_MESSAGE.SUMMARY} ${bestSolution.price}`)
 }
 
-function displayOutput(algorithmCase){
-  
-}
+calculateSolution(firstCase.listings, firstCase.neededContainer)
 
-selectContainers(cases.firstCase);
+module.exports = {calculateSolution, optimizeSolution} 
